d0ead16d66e331eb1746a05cb926a283a375f576
Giovanni Minus <giovanni19minus98@gmail.com>
2019-09-22 23:51:27 -0400

    Challenge 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text livesText;
+    public Text winText;
+    public Text loseText;
+
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>   ();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText();
+        SetLivesText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce (movement * speed);
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+
+        if (count == 12)
+        {
+            transform.position = new Vector2(55.96f, 0.0f);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            Destroy(gameObject);
+            winText.text = "You Win! Created by Giovanni Minus :)";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+            loseText.text = "You lose! Created by Giovanni Minus :(";
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
